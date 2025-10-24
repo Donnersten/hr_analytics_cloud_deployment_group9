78e36d6f45a57da5a9d05dd275cc3ea2cdfc5fc8
Donnersten <hampus@donnersten.se>
2025-10-24 10:56:07 +0200

    fixed dashboard

--- dashboard/connect_data_warehouse.py
+++ dashboard/connect_data_warehouse.py
@@ -8,15 +8,9 @@ load_dotenv()
 
 DB_PATH = os.getenv("DUCKDB_PATH")
  
-def query_job_listings(tabel_name='marts.mart_socialjob'):
+def query_job_listings(tabel_name='marts.mart_social_job'):
     query = f'SELECT * FROM {tabel_name}'
     with duckdb.connect(DB_PATH, read_only=True) as conn:
-    
-       
         # return conn.query(f"{query}").df()
         df = pd.read_sql(query, conn)
-
         return df
-if __name__ == "__main__":
-    df = query_job_listings()
-    print(df)
