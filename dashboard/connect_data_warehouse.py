c4bc2c224ce23ea87cc63d23f579078bba0c82e4
Henrik <sjogren.henrik@gmail.com>
2025-10-24 13:55:17 +0200

    azure deplyment

--- dashboard/connect_data_warehouse.py
+++ dashboard/connect_data_warehouse.py
@@ -3,11 +3,11 @@ import duckdb
 import os 
 import pandas as pd
 
-DB_PATH = os.getenv("DUCKDB_PATH")
+FILES_SHARE_PATH = Path("/mnt/data/job_ads.duckdb")
  
 def query_job_listings(tabel_name='marts.mart_social_job'):
     query = f'SELECT * FROM {tabel_name}'
-    with duckdb.connect(DB_PATH, read_only=True) as conn:
+    with duckdb.connect(FILES_SHARE_PATH, read_only=True) as conn:
         # return conn.query(f"{query}").df()
         df = pd.read_sql(query, conn)
         return df
